269e9e78ff79db0c9bd3152232162e2a2b8a22b7
Alper Altay <alper.altay20@gmail.com>
2019-12-02 12:07:51 +0100

    fiks

--- src/main/java/com/system/controller/HomeController.java
+++ src/main/java/com/system/controller/HomeController.java
@@ -2,11 +2,13 @@ package com.system.controller;
 
 
 import com.system.model.Assignment;
-import com.system.model.PropertyType;
+import com.system.model.Notice;
 import com.system.model.User;
 import com.system.service.AssignmentImp;
+import com.system.service.NoticeServiceImp;
 import com.system.service.UserServiceImp;
 import com.system.tools.TimeMessageGenerator;
+import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.security.core.Authentication;
@@ -28,11 +30,16 @@ public class HomeController {
     @Autowired
     AssignmentImp assignmentImp;
 
+    @Autowired
+    NoticeServiceImp noticeServiceImp;
+
 
 @RequestMapping( value = {"/home"}, method = RequestMethod.GET)
-    public ModelAndView home (Model model, Authentication authentication, Assignment assignment){
+    public ModelAndView home (Model model, Authentication authentication){
     User user = userServiceImp.currentUser(authentication.getName());
     TimeMessageGenerator timeMessageGenerator = new TimeMessageGenerator();
+
+    noticeServiceImp.createNotice(user,"1998-08-98", "kæmpe pik" );
 
     model.addAttribute("user", user.getName());
     model.addAttribute("timeMessage", timeMessageGenerator.timeOfTheDay());
@@ -64,13 +71,20 @@ public class HomeController {
     return modelAndView;
     }
 
-    @RequestMapping (value = "/save", method = RequestMethod.POST)
-    public ModelAndView postAssignmentFormPage(@ModelAttribute("Assignment") Assignment assignment, @ModelAttribute("propertyType")PropertyType propertyType, HttpServletRequest httpServletRequest){
 
-        assignmentImp.createAssignment(assignment.getDescription(),assignment.getStreetName(),assignment.getStreetNumber(),assignment.getCity(),assignment.getZip(),
-                assignment.getFloor(),assignment.getAssignmentDate(),assignment.getStove(),assignment.getFridge(),assignment.getWashingMachine(),assignment.getDishWasher(),assignment.getCarpets(),assignment.getCarpetTape(),
-                assignment.getBoltsAndScrews(),assignment.getCurtains(),assignment.getCurtainrod(),assignment.getBlinds(),assignment.getLamps(),assignment.getPaintings(),assignment.getDocuments(),assignment.getKeys(),assignment.getCellarAndLoft(),
-                assignment.getCleaningService(),assignment.getAccessibilityTools(),assignment.getReturnKeys(),propertyType.getType());
-        return new ModelAndView("redirect:/home");
+
+    @RequestMapping (value = {"/noticeFormPage"}, method = RequestMethod.GET)
+    public ModelAndView noticeFormpage(){
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("noticeFormPage");
+    return modelAndView;
     }
+
+    @RequestMapping (value = "/save", method = RequestMethod.POST)
+    public ModelAndView postNoticeFormPage (@ModelAttribute("notice") Notice notice, @ModelAttribute("user") User user1, Authentication authentication, HttpServletRequest httpServletRequest){
+    User user = userServiceImp.currentUser(authentication.getName());
+    noticeServiceImp.createNotice(user,"1998-08-98", "kæmpe pik" );
+    return new ModelAndView("redirect:/home");
+    }
+
 }
