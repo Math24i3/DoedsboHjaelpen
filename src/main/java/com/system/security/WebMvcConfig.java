93e1686a006474b9a7831271e56f10503e31d962
Mathias <feldt.mathias@hotmail.com>
2020-01-25 16:21:06 +0100

    fejlrettelsewr3

--- src/main/java/com/system/security/WebMvcConfig.java
+++ src/main/java/com/system/security/WebMvcConfig.java
@@ -2,18 +2,8 @@ package com.system.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mobile.device.DeviceHandlerMethodArgumentResolver;
-import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
-import org.springframework.mobile.device.site.SitePreferenceHandlerInterceptor;
-import org.springframework.mobile.device.site.SitePreferenceHandlerMethodArgumentResolver;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
-
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
